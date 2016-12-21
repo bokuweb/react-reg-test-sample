3544306d102d6e0a3c3052a9cbc5242f955b7be3
bokuweb <bokuweb12@gmail.com>
2016-12-22 01:21:30 +0900

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class HelloWorld extends Component {
+  render() {
+    return (
+      <h1 style={{ color: '#fff', fontFamily: 'arial' }} >hello</h1>
+    );
+  }
+}
